bef769912cd8aee15fa1ead96d8a638fd1f31908
maribelcuales <maribelcuales@gmail.com>
2021-02-21 20:28:28 +0800

    Add useState hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  useState();
+  
   return (
     <div className="App">
       <header className="App-header">
