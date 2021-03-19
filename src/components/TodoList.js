edcba11722927e624479103e108ea6226b7a14cb
maribelcuales <maribelcuales@gmail.com>
2021-03-19 08:34:24 +0800

    Map over the todos list and return Todo component.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+import Todo from "./Todo";
 
 function TodoList({ todos }) {
   return (
     <ul>
-
+      {todos.map(todo => (
+        <Todo todo={} />
+      ))}
     </ul>
   );
 }
 
 export default TodoList;
-
