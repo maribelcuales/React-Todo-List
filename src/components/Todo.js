3786a23be7daa7b8050ee0aed25733fb7b4284be
maribelcuales <maribelcuales@gmail.com>
2021-04-08 20:39:42 +0800

    Added style color and textDecoration to list items.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,14 @@ function Todo({ todo }) {
   return (
     <div style={{ display: "flex" }}>
       <input type="checkbox"/>
-      <li>{todo.task}</li>
+      <li
+        style={{
+          color: "white",
+          textDecoration: todo.completed ? "line-through" : null
+        }}      
+      >
+        {todo.task}
+      </li>
       <button>X</button>
     </div>
   );
