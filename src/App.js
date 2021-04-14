bd7b0633df620b8805ceafbb7ff1d8ae728f9af9
maribelcuales <maribelcuales@gmail.com>
2021-04-14 12:15:35 +0800

    Added toggle complete function.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,21 @@ function App() {
 
   function addTodo(todo) {
     setTodos([todo, ...todos]);
-  }
+  };
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo; 
+      })
+    );
+  };
   
   return (
     <div className="App">
