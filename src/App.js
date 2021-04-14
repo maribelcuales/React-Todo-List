1083e6eca280c1368ffd471e0cc89c19e704b68c
maribelcuales <maribelcuales@gmail.com>
2021-04-14 11:06:04 +0800

    Import useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
