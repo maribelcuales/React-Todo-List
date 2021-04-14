fc7ac56298e6b88442fbe9663551f7ea67f5ff0e
maribelcuales <maribelcuales@gmail.com>
2021-04-14 12:01:29 +0800

    Parse the string returned and call setTodos if it isn't null.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    const storageTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
   }, []);
 
   useEffect(() => {
