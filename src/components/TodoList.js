eec672d675049337c5cbbb50b833c23987097f59
maribelcuales <maribelcuales@gmail.com>
2021-04-30 21:58:59 +0800

    Destructure toggleComplete function and pass it to the Todo component.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, toggleComplete }) {
   return (
     <ul>
       {todos.map(todo => (
-        <Todo key={todo.id} todo={todo} />
+        <Todo key={todo.id} todo={todo} toggleComplete={toggleComplete} />
       ))}
     </ul>
   );
