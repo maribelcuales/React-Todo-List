58baf75bee5621192d949cac9bdc8e6b52c2b6f7
maribelcuales <maribelcuales@gmail.com>
2021-04-30 21:52:05 +0800

    Pass toggleComplete function to the TodoList component.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
       <header className="App-header">
         <p>React Todo</p>
         <TodoForm addTodo={addTodo} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} />
       </header>
     </div>
   );
